02e82456f2bd83ed58572fee77e882030aa5fd55
redevv <david3991rey@gmail.com>
2022-04-25 13:29:58 +0200

    Programas Python

---
+++ primerprograma.py
@@ -0,0 +1,21 @@
+# coding=<encoding name>
+# Comentarios
+# Este es un primer programa en Python
+
+textoUnalinea = "Este texto se asigna en una linea"
+
+textdistintaslineas = """Escribir valores alfanumericos 
+en distintas lineas"""
+
+# Declaraciones de variables
+numero = 10
+Numero = 20
+saludo = "Hola mundo"
+print(numero)
+print(Numero)
+print(numero+Numero)
+print("saludos"+saludo)
+print(type(numero))
+print(type(saludo))
+# del numero
+print(numero)
---
+++ segundoprograma.py
@@ -0,0 +1,12 @@
+# Asignacion simultanea
+a=5
+b=10
+print("Paso 1. Valores iniciales")
+print(a)
+print(b)
+c=b
+b=a
+a=c
+del c
+print(a)
+print(b)
