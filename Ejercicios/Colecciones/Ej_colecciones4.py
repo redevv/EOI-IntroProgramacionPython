30425d1675e8c854b816babbb36482f3fe5846b7
redevv <david3991rey@gmail.com>
2022-05-10 10:52:49 +0200

    Cambios colecciones

--- Ejercicios/Colecciones/Ej_colecciones4.py
+++ Ejercicios/Colecciones/Ej_colecciones4.py
@@ -1,6 +1,7 @@
 # Ejercicio Colecciones 4
 
 from random import randint
+from statistics import mean
 
 NombresCiudades={"Lugo", "Pontevedra", "Gijón", "Santander", "Bilbao", "Álava", "Pamplona", "Tarragona", "Barcelona", "Castellón", "Alicante","Murcia","Albacete", "Cuenca", "Toledo", "Madrid", "Segovia", "Salamanca", "Burgos", "León"}
 
